2ed4813b74895c5e3259401c60c1fe67e7ad6391
Shoktapus <sinclair8075@gmail.com>
2022-07-19 21:39:25 +0000

    Began the framework for an excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,11 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["aliens", "donkeys", "barbarians"];
+  let actions = ["vaporized", "dissolved", "maimed"];
+  let what = ["my wife", "my work", "my therapist"];
+  let when = ["a minute ago", "last night", "this morning"];
+  let sentence = who[0] + " " + actions[0] + " " + what[0] + " " + when[0];
+  let element = document.getElementById("excuse");
+  element.innerHTML = sentence;
 };
