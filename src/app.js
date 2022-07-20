b1af9b3a89e6cb44f1b6e3bb09950f102522adcd
Shoktapus <sinclair8075@gmail.com>
2022-07-20 19:11:04 +0000

    Finished product.

--- src/app.js
+++ src/app.js
@@ -7,11 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let who = ["aliens", "donkeys", "barbarians"];
-  let actions = ["vaporized", "dissolved", "maimed"];
-  let what = ["my wife", "my work", "my therapist"];
-  let when = ["a minute ago", "last night", "this morning"];
-  let sentence = who[0] + " " + actions[0] + " " + what[0] + " " + when[0];
+  let who = [
+    "Aliens",
+    "Cyborgs",
+    "Supes",
+    "An Extinction Level Event",
+    "Sharks With Lasers"
+  ];
+  let actions = ["vaporized", "dissolved", "maimed", "liquefied", "atomized"];
+  let what = ["my wife", "my work", "my therapist", "my truck", "my apartment"];
+  let when = [
+    "a minute ago",
+    "last night",
+    "this morning",
+    "yesterday",
+    "just now"
+  ];
+
+  function getRandomNum(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+  let whoIndex = getRandomNum(who);
+  let actionsIndex = getRandomNum(actions);
+  let whatIndex = getRandomNum(what);
+  let whenIndex = getRandomNum(when);
+  let sentence =
+    who[whoIndex] +
+    " " +
+    actions[actionsIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex] +
+    ".";
+
   let element = document.getElementById("excuse");
   element.innerHTML = sentence;
 };
